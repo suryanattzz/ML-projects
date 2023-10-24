7cb24c77bd3a14316083e262f7368cac3578f1cf
Surya Nattzz <118332725+suryanattzz@users.noreply.github.com>
2023-10-24 12:41:43 +0530

    Add files via upload

---
+++ heart_disease_pred_webapp.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 12 22:45:31 2022
+
+@author: SRIJITH
+"""
+
+import numpy as np
+import pickle
+import streamlit as st
+
+
+#loading the saved model
+loaded_model=pickle.load(open('C:/Users/SRIJITH/Documents/internship_project/heart_disease_pred/trained_model.sav','rb'))
+
+
+
+#creating a function for prediction
+def heart_disease_prediction(input_data):
+    #input_data=(52,1,0,125,212,0,1,168,0,1.0,2,2,3)
+    input_data_as_numpy_array = np.asarray(input_data)
+    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+    prediction = loaded_model.predict(input_data_reshaped)
+    print(prediction)
+    
+    if (prediction[0] == 0):
+        return "Person don't have heart disease"
+    else:
+         return "Person have heart disease"
+     
+        
+def main():
+    #giving a title
+    st.title("Heart Disease Prediction Web App")
+    
+    #getting the input data from the user
+    #age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal
+    
+    age = st.text_input('Age')
+    sex = st.text_input('Sex')
+    cp = st.text_input('Chest Pain Type')
+    trestbps = st.text_input('Resting Blood Pressure in mm')
+    chol = st.text_input('serum cholesterol in mg/dl')
+    fbs = st.text_input('fasting blood sugar > 120 mg/dl (1=true; 0=false)')
+    restecg = st.text_input('resting electrocardiographic results')
+    thalach = st.text_input('maximum heart rate achieved')
+    exang = st.text_input('exercise-induced angina')
+    oldpeak = st.text_input('ST depression induced by exercise relative to rest')
+    slope = st.text_input('the slope of the peak exercise ST segment')
+    ca = st.text_input('number of major vessels (0–3) colored by fluorosopy')
+    thal = st.text_input('thalassemia')
+    
+    #code for prediction
+    
+    diagnosis = ''
+    
+    #creating a button for prediction
+    if st.button('Heart Disease Test Result'):
+        diagnosis = heart_disease_prediction([age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal])
+        
+    st.success(diagnosis)
+    
+    
+    
+    
+if __name__ == '__main__':
+    main()
+    
+    
+    
+    
+    
+    
+    
